78c955ef65d21a76990b953804e461b96588a368
Diego <diego2000avelar@gmail.com>
2021-08-13 19:42:04 -0600

    chakra ui setup

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import * as React from "react"
+import Header from "./layout/header"
+// 1. import `ChakraProvider` component
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+    brand: {
+      900: "#1a365d",
+      800: "#153e75",
+      700: "#2a69ac",
+    },
+  }
+  const theme = extendTheme({ colors })
 
-const App = () => {
-    return (
-        <div>
-            Blog App
-        </div>
-    )
+export default function App( ) {
+  // 2. Use at the root of your app
+  return (
+    <ChakraProvider theme={theme}>
+        <Header/>
+      Blog app
+    </ChakraProvider>
+  )
 }
-
-export default App
