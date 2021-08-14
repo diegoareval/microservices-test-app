d6c139908e335c3e467b7c117cea87b323f86ac5
Diego <diego2000avelar@gmail.com>
2021-08-14 10:43:34 -0600

    comments

--- client-app/src/lib/api.js
+++ client-app/src/lib/api.js
@@ -27,5 +27,32 @@ export default class Api {
                 data: []
             }});
     }
+
+    static getCommentsByPost (id){
+        return axios.get(`http://localhost:4000/posts/${id}/comments`).then((res)=> {
+            return {
+                status: true,
+                data: res.data
+            }
+        }).catch(err => {console.error(err) 
+            return {
+                status: false,
+                data: []
+            }});
+        }
+
+        static createPostComment (content, id){
+            return axios.post(`http://localhost:4000/posts/${id}/comments`, {
+                content
+            }).then((res)=> {
+                return {
+                    status: true,
+                    data: res.data
+                }
+            }).catch(err => {console.error(err) 
+                return {
+                    status: false
+                }});
+            }
    
 }
